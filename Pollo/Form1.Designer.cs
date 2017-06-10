f7bc9a651f11105350a1633531eeba69dc2fb4bf
minhphong306 <dominhphong306@gmail.com>
2017-06-10 22:06:54 +0700

    Read + write file

--- Pollo/Form1.Designer.cs
+++ Pollo/Form1.Designer.cs
@@ -24,6 +24,7 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lbxToDo = new System.Windows.Forms.ListBox();
             this.contextOption = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.contextToDo = new System.Windows.Forms.ToolStripMenuItem();
@@ -36,13 +37,13 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txtAddToDo = new Bunifu.Framework.UI.BunifuMetroTextbox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.bunifuMetroTextbox1 = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.txtAddDoing = new Bunifu.Framework.UI.BunifuMetroTextbox();
             this.lbxDoing = new System.Windows.Forms.ListBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.bunifuMetroTextbox2 = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.txtAddDone = new Bunifu.Framework.UI.BunifuMetroTextbox();
             this.lbxDone = new System.Windows.Forms.ListBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.bunifuMetroTextbox3 = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.txtBlock = new Bunifu.Framework.UI.BunifuMetroTextbox();
             this.lbxBlock = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -59,9 +60,6 @@
             this.lbxToDo.ContextMenuStrip = this.contextOption;
             this.lbxToDo.FormattingEnabled = true;
             this.lbxToDo.ItemHeight = 20;
-            this.lbxToDo.Items.AddRange(new object[] {
-            "Đọc Conan",
-            "Fix bug connection string"});
             this.lbxToDo.Location = new System.Drawing.Point(6, 85);
             this.lbxToDo.Name = "lbxToDo";
             this.lbxToDo.Size = new System.Drawing.Size(240, 324);
@@ -158,7 +156,7 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.bunifuMetroTextbox1);
+            this.groupBox2.Controls.Add(this.txtAddDoing);
             this.groupBox2.Controls.Add(this.lbxDoing);
             this.groupBox2.Location = new System.Drawing.Point(284, 40);
             this.groupBox2.Name = "groupBox2";
@@ -167,33 +165,29 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Đang làm";
             // 
-            // bunifuMetroTextbox1
+            // txtAddDoing
             // 
-            this.bunifuMetroTextbox1.BorderColorFocused = System.Drawing.Color.Blue;
-            this.bunifuMetroTextbox1.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.bunifuMetroTextbox1.BorderColorMouseHover = System.Drawing.Color.Blue;
-            this.bunifuMetroTextbox1.BorderThickness = 3;
-            this.bunifuMetroTextbox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.bunifuMetroTextbox1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.bunifuMetroTextbox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.bunifuMetroTextbox1.isPassword = false;
-            this.bunifuMetroTextbox1.Location = new System.Drawing.Point(7, 25);
-            this.bunifuMetroTextbox1.Margin = new System.Windows.Forms.Padding(4);
-            this.bunifuMetroTextbox1.Name = "bunifuMetroTextbox1";
-            this.bunifuMetroTextbox1.Size = new System.Drawing.Size(239, 44);
-            this.bunifuMetroTextbox1.TabIndex = 1;
-            this.bunifuMetroTextbox1.Text = "Thêm việc đang làm";
-            this.bunifuMetroTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.bunifuMetroTextbox1.Enter += new System.EventHandler(this.txtAddToDo_Enter);
-            this.bunifuMetroTextbox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAddToDo_KeyDown);
-            this.bunifuMetroTextbox1.Leave += new System.EventHandler(this.txtAddToDo_Leave);
+            this.txtAddDoing.BorderColorFocused = System.Drawing.Color.Blue;
+            this.txtAddDoing.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtAddDoing.BorderColorMouseHover = System.Drawing.Color.Blue;
+            this.txtAddDoing.BorderThickness = 3;
+            this.txtAddDoing.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtAddDoing.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtAddDoing.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtAddDoing.isPassword = false;
+            this.txtAddDoing.Location = new System.Drawing.Point(7, 25);
+            this.txtAddDoing.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAddDoing.Name = "txtAddDoing";
+            this.txtAddDoing.Size = new System.Drawing.Size(239, 44);
+            this.txtAddDoing.TabIndex = 1;
+            this.txtAddDoing.Text = "Thêm việc đang làm";
+            this.txtAddDoing.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtAddDoing.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAddDoing_KeyDown);
             // 
             // lbxDoing
             // 
             this.lbxDoing.FormattingEnabled = true;
             this.lbxDoing.ItemHeight = 20;
-            this.lbxDoing.Items.AddRange(new object[] {
-            "Ứng dụng destop WALLO"});
             this.lbxDoing.Location = new System.Drawing.Point(6, 85);
             this.lbxDoing.Name = "lbxDoing";
             this.lbxDoing.Size = new System.Drawing.Size(240, 324);
@@ -201,7 +195,7 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.bunifuMetroTextbox2);
+            this.groupBox3.Controls.Add(this.txtAddDone);
             this.groupBox3.Controls.Add(this.lbxDone);
             this.groupBox3.Location = new System.Drawing.Point(553, 40);
             this.groupBox3.Name = "groupBox3";
@@ -210,34 +204,28 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Đã làm";
             // 
-            // bunifuMetroTextbox2
+            // txtAddDone
             // 
-            this.bunifuMetroTextbox2.BorderColorFocused = System.Drawing.Color.Blue;
-            this.bunifuMetroTextbox2.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.bunifuMetroTextbox2.BorderColorMouseHover = System.Drawing.Color.Blue;
-            this.bunifuMetroTextbox2.BorderThickness = 3;
-            this.bunifuMetroTextbox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.bunifuMetroTextbox2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.bunifuMetroTextbox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.bunifuMetroTextbox2.isPassword = false;
-            this.bunifuMetroTextbox2.Location = new System.Drawing.Point(5, 25);
-            this.bunifuMetroTextbox2.Margin = new System.Windows.Forms.Padding(4);
-            this.bunifuMetroTextbox2.Name = "bunifuMetroTextbox2";
-            this.bunifuMetroTextbox2.Size = new System.Drawing.Size(239, 44);
-            this.bunifuMetroTextbox2.TabIndex = 1;
-            this.bunifuMetroTextbox2.Text = "Thêm việc đã làm";
-            this.bunifuMetroTextbox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.bunifuMetroTextbox2.Enter += new System.EventHandler(this.txtAddToDo_Enter);
-            this.bunifuMetroTextbox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAddToDo_KeyDown);
-            this.bunifuMetroTextbox2.Leave += new System.EventHandler(this.txtAddToDo_Leave);
+            this.txtAddDone.BorderColorFocused = System.Drawing.Color.Blue;
+            this.txtAddDone.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtAddDone.BorderColorMouseHover = System.Drawing.Color.Blue;
+            this.txtAddDone.BorderThickness = 3;
+            this.txtAddDone.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtAddDone.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtAddDone.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtAddDone.isPassword = false;
+            this.txtAddDone.Location = new System.Drawing.Point(5, 25);
+            this.txtAddDone.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAddDone.Name = "txtAddDone";
+            this.txtAddDone.Size = new System.Drawing.Size(239, 44);
+            this.txtAddDone.TabIndex = 1;
+            this.txtAddDone.Text = "Thêm việc đã làm";
+            this.txtAddDone.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
             // lbxDone
             // 
             this.lbxDone.FormattingEnabled = true;
             this.lbxDone.ItemHeight = 20;
-            this.lbxDone.Items.AddRange(new object[] {
-            "Crawl dữ liệu",
-            "Đọc \"Code dạo kí sự\""});
             this.lbxDone.Location = new System.Drawing.Point(6, 85);
             this.lbxDone.Name = "lbxDone";
             this.lbxDone.Size = new System.Drawing.Size(240, 324);
@@ -245,7 +233,7 @@
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.bunifuMetroTextbox3);
+            this.groupBox4.Controls.Add(this.txtBlock);
             this.groupBox4.Controls.Add(this.lbxBlock);
             this.groupBox4.Location = new System.Drawing.Point(817, 79);
             this.groupBox4.Name = "groupBox4";
@@ -254,33 +242,28 @@
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Bị lỡ";
             // 
-            // bunifuMetroTextbox3
+            // txtBlock
             // 
-            this.bunifuMetroTextbox3.BorderColorFocused = System.Drawing.Color.Blue;
-            this.bunifuMetroTextbox3.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.bunifuMetroTextbox3.BorderColorMouseHover = System.Drawing.Color.Blue;
-            this.bunifuMetroTextbox3.BorderThickness = 3;
-            this.bunifuMetroTextbox3.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.bunifuMetroTextbox3.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.bunifuMetroTextbox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.bunifuMetroTextbox3.isPassword = false;
-            this.bunifuMetroTextbox3.Location = new System.Drawing.Point(7, 25);
-            this.bunifuMetroTextbox3.Margin = new System.Windows.Forms.Padding(4);
-            this.bunifuMetroTextbox3.Name = "bunifuMetroTextbox3";
-            this.bunifuMetroTextbox3.Size = new System.Drawing.Size(239, 44);
-            this.bunifuMetroTextbox3.TabIndex = 1;
-            this.bunifuMetroTextbox3.Text = "Thêm việc bị lỡ";
-            this.bunifuMetroTextbox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.bunifuMetroTextbox3.Enter += new System.EventHandler(this.txtAddToDo_Enter);
-            this.bunifuMetroTextbox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAddToDo_KeyDown);
-            this.bunifuMetroTextbox3.Leave += new System.EventHandler(this.txtAddToDo_Leave);
+            this.txtBlock.BorderColorFocused = System.Drawing.Color.Blue;
+            this.txtBlock.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtBlock.BorderColorMouseHover = System.Drawing.Color.Blue;
+            this.txtBlock.BorderThickness = 3;
+            this.txtBlock.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBlock.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtBlock.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtBlock.isPassword = false;
+            this.txtBlock.Location = new System.Drawing.Point(7, 25);
+            this.txtBlock.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBlock.Name = "txtBlock";
+            this.txtBlock.Size = new System.Drawing.Size(239, 44);
+            this.txtBlock.TabIndex = 1;
+            this.txtBlock.Text = "Thêm việc bị lỡ";
+            this.txtBlock.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
             // lbxBlock
             // 
             this.lbxBlock.FormattingEnabled = true;
             this.lbxBlock.ItemHeight = 20;
-            this.lbxBlock.Items.AddRange(new object[] {
-            "Chat realtime firebase"});
             this.lbxBlock.Location = new System.Drawing.Point(6, 85);
             this.lbxBlock.Name = "lbxBlock";
             this.lbxBlock.Size = new System.Drawing.Size(240, 284);
@@ -308,7 +291,8 @@
             // 
             // notifyIcon1
             // 
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Wallo";
             this.notifyIcon1.Visible = true;
             // 
             // Form1
@@ -325,6 +309,9 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.contextOption.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
@@ -352,9 +339,9 @@
         private System.Windows.Forms.ToolStripMenuItem đãLàmToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bịLỡToolStripMenuItem;
         private Bunifu.Framework.UI.BunifuMetroTextbox txtAddToDo;
-        private Bunifu.Framework.UI.BunifuMetroTextbox bunifuMetroTextbox1;
-        private Bunifu.Framework.UI.BunifuMetroTextbox bunifuMetroTextbox2;
-        private Bunifu.Framework.UI.BunifuMetroTextbox bunifuMetroTextbox3;
+        private Bunifu.Framework.UI.BunifuMetroTextbox txtAddDoing;
+        private Bunifu.Framework.UI.BunifuMetroTextbox txtAddDone;
+        private Bunifu.Framework.UI.BunifuMetroTextbox txtBlock;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
--- Pollo/Form1.cs
+++ Pollo/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,28 +11,69 @@ using System.Windows.Forms;
 
 namespace Pollo {
     public partial class Form1 : Form {
+        private int MODE;
+        private const int TODO = 1, DOING = 2, DONE = 3, BLOCK = 4;
+        string FILE_HEADER = "D:\\WALLO\\";
+        string FILE_TRAILER = ".dat";
 
         public Form1() {
             InitializeComponent();
             this.StartPosition = FormStartPosition.CenterScreen;
-            // Set up how the form should be displayed.
-            this.ClientSize = new System.Drawing.Size(292, 266);
-            this.Text = "Notify Icon Example";
+        }
 
-            // Create the NotifyIcon.
-            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+        private void readFile(string filePath, int mode) {
+            //"D:\\WALLO\\ToDo.dat"
+            if (!File.Exists(filePath)) {
+                File.Create(filePath).Dispose();
+            }
+            using (System.IO.StreamReader r = new System.IO.StreamReader(filePath)) {
+                string line;
+                while ((line = r.ReadLine()) != null) {
+                    switch (mode) {
+                        case TODO:
+                            lbxToDo.Items.Add(line);
+                            break;
+                        case DOING:
+                            lbxDoing.Items.Add(line);
+                            break;
+                        case DONE:
+                            lbxDone.Items.Add(line);
+                            break;
+                        case BLOCK:
+                            lbxBlock.Items.Add(line);
+                            break;
+                    }
 
-            // The Icon property sets the icon that will appear
-            // in the systray for this application.
-            notifyIcon1.Icon = new Icon("appicon.ico");
+                }
+            }
+        }
 
-            // The ContextMenu property sets the menu that will
+        private void writeFile(string filePath, int mode) {
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(filePath);
+            switch (mode) {
+                case TODO:
+                    foreach (var item in lbxToDo.Items) {
+                        SaveFile.WriteLine(item.ToString());
+                    }
+                    break;
+                case DOING:
+                    foreach (var item in lbxDoing.Items) {
+                        SaveFile.WriteLine(item.ToString());
+                    }
+                    break;
+                case DONE:
+                    foreach (var item in lbxDone.Items) {
+                        SaveFile.WriteLine(item.ToString());
+                    }
+                    break;
+                case BLOCK:
+                    foreach (var item in lbxBlock.Items) {
+                        SaveFile.WriteLine(item.ToString());
+                    }
+                    break;
+            }
 
-            // The Text property sets the text that will be displayed,
-            // in a tooltip, when the mouse hovers over the systray icon.
-            notifyIcon1.Text = "Form1 (NotifyIcon example)";
-            notifyIcon1.Visible = true;
-
+            SaveFile.Close();
         }
 
         private void panel1_DragDrop(object sender, DragEventArgs e) {
@@ -71,21 +113,42 @@ namespace Pollo {
 
         private void contextRemove_Click(object sender, EventArgs e) {
             int index = lbxToDo.SelectedIndex;
-            if (index == -1)
-            {
+            if (index == -1) {
                 return;
             }
             lbxToDo.Items.RemoveAt(index);
         }
 
-        private void button1_Click(object sender, EventArgs e) {
-            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter("D:\\WALLO\\ToDo.dat");
-            foreach (var item in lbxToDo.Items) {
-                SaveFile.WriteLine(item.ToString());
+        private void txtAddDoing_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                String item = txtAddDoing.Text;
+                if ("".Equals(item)) {
+                    return;
+                }
+                lbxDoing.Items.Add(item);
+                txtAddDoing.Text = "";
             }
-            SaveFile.Close();
-            MessageBox.Show("Success");
-            
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Application.Exit();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            notifyIcon1.ShowBalloonTip(1000, "Thông báo", "Chương trình Wallo đã được khởi động", ToolTipIcon.Info);
+
+            readFile(FILE_HEADER + "ToDo" + FILE_TRAILER, TODO);
+            readFile(FILE_HEADER + "Doing" + FILE_TRAILER, DOING);
+            readFile(FILE_HEADER + "Done" + FILE_TRAILER, DONE);
+            readFile(FILE_HEADER + "Block" + FILE_TRAILER, BLOCK);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            writeFile(FILE_HEADER + "ToDo" + FILE_TRAILER, TODO);
+            writeFile(FILE_HEADER + "Doing" + FILE_TRAILER, DOING);
+            writeFile(FILE_HEADER + "Done" + FILE_TRAILER, DONE);
+            writeFile(FILE_HEADER + "Block" + FILE_TRAILER, BLOCK);
         }
     }
 }
